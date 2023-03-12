4919c4017d29e484efbb0181e06c6dbd9b8afd85
d3m0nk3y <aayushmaharjan20@gmail.com>
2023-03-12 07:19:00 +0545

    middleware fix

--- middleware/athentication.js
+++ middleware/athentication.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers["token"];
+  // const token = req.headers["token"];
+  const token = req.headers.authorization.split(" ")[1];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
